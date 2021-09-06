43f70211cb892a2061f576cfe9e63f1725e64501
Jair <jairaceves56@gmail.com>
2021-09-06 15:35:43 -0500

    le agregamos las barras identificadoras

---
+++ incluides/nav.php
@@ -0,0 +1,14 @@
+<nav>
+    <ul>
+        <li><a href="index.php"> Home</a></li>
+        <li class="principal">
+            <a href="productosp.php"> Productos</a>
+        </li>
+        <li class="principal">
+            <a href="contacto.php">Contacto</a>
+        </li>
+
+        <li class="principal">
+            <a href="carrito.php"> Carrito</a>
+        </li>
+</nav>
