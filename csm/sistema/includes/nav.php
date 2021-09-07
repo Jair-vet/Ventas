bb80246ff1013c954d73239412f1bfd1dca2d735
Jair <jairaceves56@gmail.com>
2021-09-07 11:16:05 -0500

    creamos la barra de menu del sistema

---
+++ csm/sistema/includes/nav.php
@@ -0,0 +1,69 @@
+<nav>
+			<ul>
+				<li><a href="index.php"><i class="fas fa-home"></i> Inicio</a></li>
+				<li class="principal">
+			<?php
+				if($_SESSION['rol'] == 1){
+			?>
+					<a href="#"><i class="fas fa-user"></i> Administradores</a>
+						<ul>
+							<li><a href="registro_usuario.php"><i class="fas fa-user-plus"></i> Nuevo Administrador</a></li>
+							<li><a href="lista_usuarios.php"><i class="far fa-list-alt"></i> Lista de Administradores</a></li>
+						</ul>
+					</li>
+			<?php }?>
+			<?php
+				if($_SESSION['rol'] == 1 || $_SESSION['rol'] == 2 ){
+			?>
+				<li class="principal">
+					<a href="#"><i class="fas fa-users"></i> Clientes</a>
+					<ul>
+						<li><a href="registro_cliente.php"><i class="fas fa-user-plus"></i> Nuevo Cliente</a></li>
+						<li><a href="lista_clientes.php"><i class="far fa-list-alt"></i> Lista de Clientes</a></li>
+					</ul>
+				</li>
+			<?php } ?>
+
+			<?php
+				if($_SESSION['rol'] == 1 || $_SESSION['rol'] == 2 ){
+			?>
+				<li class="principal">
+					<a href="#"><i class="far fa-building"></i> Proveedores</a>
+					<ul>
+						<li><a href="registro_proveedor.php"><i class="fas fa-plus"></i> Nuevo Proveedor</a></li>
+						<li><a href="lista_proveedores.php"><i class="far fa-list-alt"></i> Lista de Proveedores</a></li>
+					</ul>
+				</li>
+			<?php } ?>
+
+			<?php
+				if($_SESSION['rol'] == 1 || $_SESSION['rol'] == 2 ){
+			?>
+				<li class="principal">
+					<a href="#"><i class="fas fa-cubes"></i> Productos</a>
+					<ul>
+						<li><a href="registro_producto.php"><i class="fas fa-plus"></i> Nuevo Producto</a></li>
+						<li><a href="lista_producto.php"><i class="fas fa-cube"></i> Lista de Productos</a></li>
+					</ul>
+				</li>
+			<?php } ?>
+				<li class="principal">
+					<a href="#"><i class="fab fa-amazon-pay fa-1x"></i> Catalogo</a>
+					<ul>
+						<li><a href="catalogo1.php"><i class="fas fa-eye"></i> Ver Productos</a></li>
+					</ul>
+				</li>
+				<?php
+				if($_SESSION['rol'] == 1){
+			?>
+				<li class="principal">
+					<a href="#"> Banner</a>
+					<ul>
+						<li><a href="banner.php"> Subir Imagenes</a></li>
+					</ul>
+				</li>
+			<?php } ?>
+			</ul>
+		</nav>
+
+		
