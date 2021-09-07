241cc26aa092c7a475aad26db7bed9887d24b4a8
Jair <jairaceves56@gmail.com>
2021-09-07 11:15:10 -0500

    creamos el encabezado del sistema

---
+++ csm/sistema/includes/header.php
@@ -0,0 +1,26 @@
+<?php
+
+	if(empty($_SESSION['active']))
+	{
+		header('location: ../');
+	}
+
+?>
+<header>
+	<div class="header">
+		<h1><i class="fab fa-aws"></i> Sistema de Ventas</h1>
+			<div class="optionsBar">
+				<p>México, <?php echo fechaC(); ?></p>
+				<span>|</span>
+				<span class="user"><?php echo $_SESSION['nombre']; ?></span>  <!-- Nombre del Usuario que Ingresa -->
+				<img class="photouser" src="img/user.png" alt="Usuario">
+				<a href="salir.php"><img class="close" src="img/salir.png" alt="Salir del sistema" title="Salir"></a>
+			</div>
+		</div>
+		<?php include "nav.php"; ?>
+</header>
+<div class="modal">
+	<div class="bodyModal">
+		
+	</div>
+</div>
